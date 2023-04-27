f988931749418f9881a9d4426ed168efcdf0b095
Dmitry Gavrilenko <gavrilaz0076@gmail.com>
2023-04-27 13:39:55 +0300

    1

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import datetime
 import json
-import io
 from operator import itemgetter
 
 file = open("operations.json")
@@ -17,7 +16,6 @@ data_sorted = sorted(data_item, key=itemgetter('date'), reverse=True)
 for i in range(5):
     if 'from' not in data_sorted[i]:
         data_sorted[i]['from'] = 'None'
-    print(f"{data_sorted[i]['date'].strftime('%d.%m.%Y')} {data_sorted[i]['description']}")
+    print(f"\n{data_sorted[i]['date'].strftime('%d.%m.%Y')} {data_sorted[i]['description']}")
     print(f"{data_sorted[i]['from']} -> {data_sorted[i]['to']}")
-    print(f"{data_sorted[i]['operationAmount']['amount']} {data_sorted[i]['operationAmount']['currency']['name']}")
-    print("")
+    print(f"{data_sorted[i]['operationAmount']['amount']} {data_sorted[i]['operationAmount']['currency']['name']}\n")
